689ab30434cdec1bd58661209ae7403f6b1e10e6
Tagir Gumerov <tigprog@gmail.com>
2019-03-12 17:07:19 +0300

    add runner.py and root

---
+++ experiment_tool/runner.py
@@ -0,0 +1,9 @@
+class Runner:
+    def __init__(self, experiment_name, dataset_name, metrics):
+        self._experiment_name = experiment_name
+        self._dataset_name = dataset_name
+        self._metrics = metrics
+
+
+if __name__ == '__main__':
+    pass
