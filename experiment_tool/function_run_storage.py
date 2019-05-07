abd4a8058d932ca9c3f4c2f7d80fd2165d17e261
andrewprudnikov23.gmail.com <andrewprudnikov23@gmail.com>
2019-05-07 11:26:47 +0300

    change sqlite storage to hash storage and add file storage for results

--- experiment_tool/function_run_storage.py
+++ experiment_tool/function_run_storage.py
@@ -1,20 +1,21 @@
 import inspect
 import logging
+import os
 import pickle
 import sqlite3
 from typing import Any, Callable, Dict
 
 from experiment_tool.utils import Maybe
 
-
 log = logging.getLogger(__name__)
 
 
 class FunctionRunInfo:
-    def __init__(self, func: Callable, args: Dict[str, Any], result=None):
-        self.code = inspect.getsource(func)
-        self.args = str(sorted(args.items(), key=lambda x: x[0]))
-        self.result = pickle.dumps(result)
+    def __init__(self, func: Callable, args: Dict[str, Any]):
+        self.code_hash = hash(inspect.getsource(func))
+        self.args_hash = hash(str(sorted(args.items(), key=lambda x: x[0])))
+        self.path = os.path.join(os.path.split(os.path.dirname(__file__))[0],
+                                 "results", str(hash((self.code_hash, self.args_hash))))
 
 
 class FunctionRunStorage:
@@ -25,26 +26,29 @@ class FunctionRunStorage:
         self._cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS functions
-            (function_code TEXT, args TEXT, result BLOB,
-            UNIQUE(function_code, args))
+            (function_code_hash INTEGER, args_hash INTEGER, result_path TEXT,
+            UNIQUE(function_code_hash, args_hash))
             """
         )
 
     def add_function(self, func: Callable, args: Dict[str, Any], result: Any):
         log.debug('start FunctionRunStorage.add_function')
-        function_run_info = FunctionRunInfo(func, args, result)
-        self._cursor.execute(
-            """
-            INSERT OR IGNORE INTO functions
-            VALUES (?, ?, ?)
-            """,
-            (
-                function_run_info.code,
-                function_run_info.args,
-                function_run_info.result
+        function_run_info = FunctionRunInfo(func, args)
+        if self.get_function_result(func, args).is_nothing():
+            with open(function_run_info.path, 'wb') as f:
+                pickle.dump(result, f)
+            self._cursor.execute(
+                """
+                INSERT INTO functions
+                VALUES (?, ?, ?)
+                """,
+                (
+                    function_run_info.code_hash,
+                    function_run_info.args_hash,
+                    function_run_info.path
+                )
             )
-        )
-        self.conn.commit()
+            self.conn.commit()
         log.debug('finish FunctionRunStorage.add_function')
 
     def get_function_result(self, func: Callable, args: Dict[str, Any]) \
@@ -53,14 +57,15 @@ class FunctionRunStorage:
         function_run_info = FunctionRunInfo(func, args)
         self._cursor.execute(
             """
-            SELECT result FROM functions
-            WHERE function_code=:code AND args=:args
+            SELECT result_path FROM functions
+            WHERE function_code_hash=:code_hash AND args_hash=:args_hash
             """,
-            {"code": function_run_info.code, "args": function_run_info.args}
+            {"code_hash": function_run_info.code_hash, "args_hash": function_run_info.args_hash}
         )
         res = self._cursor.fetchone()
         log.debug('finish FunctionRunStorage.get_function_result')
         if res is not None:
-            res = pickle.loads(res[0])
-            return Maybe(res)
+            res_path = pickle.loads(res[0])
+            with open(res_path, 'rb') as f:
+                return Maybe(pickle.load(f))
         return Maybe(nothing=True)
